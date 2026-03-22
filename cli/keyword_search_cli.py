e4f794dbfddd561473a8ff066058d314b1c233d6
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2026-03-22 20:11:50 +0100

    My version of CH1L4 exercice

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-
+import search
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -15,7 +15,9 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            pass
+            movies = search.search(args.query)
+            for i in range(len(movies)):
+                print(f"{i + 1}. {movies[i]['title']}")
         case _:
             parser.print_help()
 
